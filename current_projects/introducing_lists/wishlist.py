5be4624261551711b11e4c88233ddd1b18dd575c
Riley Ittidecharchoti <polyglotpengyou@gmail.com>
2021-06-21 23:44:46 +0700

    more list practice

---
+++ current_projects/introducing_lists/wishlist.py
@@ -0,0 +1,19 @@
+'''
+some methods: 
+.insert(0, "string")
+ .pop()
+
+'''
+
+books = [
+    "Automate the Boring Stuff with Python: Practical Programming for Total Beginners - Al Sweigart",
+    "Python for Data Analysis - Wes McKinney",
+    "Fluent Python: Clear, Concise, and Effective Programming - Luciano Ramalho",
+    "Python for Kids: A Playful Introduction To Programming - Jason R. Briggs",
+    "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
+]
+
+
+# books[len(books) -1] (to get the last book) or do books[-1]
+
+print("Suggested gift: {}".format(books[0]))
