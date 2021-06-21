48f17142124b747c4694150b7a8e099abdb04f98
Riley Ittidecharchoti <polyglotpengyou@gmail.com>
2021-06-21 14:34:52 +0700

    syncing mac and linux

---
+++ current_projects/python_basics/looping.py
@@ -0,0 +1,21 @@
+name = input("What's your name? ")
+do_you = input("Do you understand while loops? ")
+
+# TODO: Ask the user by name if they understand Python while loops
+
+
+# TODO: Write a while statement that checks if the user doesn't understand while loops
+# TODO: Since the user doesn't understand while loops, let's explain them.
+# TODO: Ask the user again, by name, if they understand while loops.
+
+  
+# TODO: Outside the while loop, congratulate the user for understanding while loops
+ 
+understand = "yes"
+not_understand = "no"
+and_now = input("Now do you get it? ")
+
+while do_you != understand.lower():
+    print("{} You dumb...come on man!".format(name))
+    do_you = input("now??") 
+print("Goodgood")
