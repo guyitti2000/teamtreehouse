ddab62bcfc6962a9bc4812aeedbb85af51a84ff6
Riley Ittidecharchoti <polyglotpengyou@gmail.com>
2021-06-24 21:00:46 +0700

    made my own class called phone

--- current_projects/obj_ori_python/cars.py
+++ current_projects/obj_ori_python/cars.py
@@ -1,34 +1,49 @@
 class Car:
-    #class attributes
+
     wheels = 4
     doors = 2
     engine = True
-#objects created in the class (instantiation) are called instances
 
+    def __init__(self, make, model, year=2020, gas=100):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.is_moving = False
+        self.gas = gas
 
-car_one = Car()
-car_two = Car()
-print(f'car_one {car_one.doors}')
-print(id(car_one.doors))
-print(f'car_two {car_two.doors}')
-print(id(car_two.doors))
-print(f'Car {Car.doors}')
-print(id(Car.doors))
+    def __str__(self):
+        return f'{self.make} {self.model} {self.year}'
 
-Car.doors = 5
-car_one.doors = 6
-print(f'car_one {car_one.doors}')
-print(id(car_one.doors))
-print(f'car_two {car_two.doors}')
-print(id(car_two.doors))
-print(f'Car {Car.doors}')
-print(id(Car.doors))
+    def use_gas(self):
+        self.gas -= 50
+        print(self.gas)
 
+    def stop(self):
+        if self.is_moving:
+            print('The car has stopped.')
+            self.is_moving = False
+        else:
+            print('The car has already stopped.')
 
+    def go(self, speed):
+        if self.use_gas():
+            if not self.is_moving:
+                print('The car starts moving.')
+                self.is_moving = True
+            print(f'The car is going {speed}.')
+        else:
+            print("You've run out of gas!")
+            self.stop()
 
-
-
-
+car_one = Car("Mustang", "Model T", 1908)
+print(car_one)
+print(str(car_one))
+car_one.use_gas()
+car_one.stop()
+car_one.go("slow")
+car_one.go("fast")
+car_one.stop()
+car_one.stop()
 
 '''
 my_car = Car()
